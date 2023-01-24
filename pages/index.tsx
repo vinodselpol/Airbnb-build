29043e262620d41528e8983dd768c1da54896528
vinodselpol <vinodselpol@gmail.com>
2023-01-24 12:31:10 -0600

    Header Update

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Airbnb Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+     
+
+      {/* Header */}
+      <Header />
+
+      { /* Banner */}
+
+      
+    </div>
+  )
+}
+
+export default Home
