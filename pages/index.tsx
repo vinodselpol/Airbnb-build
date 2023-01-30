ea6e3dce6c3a53e5192063a5b63d95003c0156c8
vinodselpol <vinodselpol@gmail.com>
2023-01-30 17:03:29 -0600

    Home Page update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
 
-const Home: NextPage = () => {
+import Head from 'next/head'
+
+import Banner from '../components/Banner'
+import Header from '../components/Header'
+import LargeCard from '../components/LargeCard'
+import MediumCard from '../components/MediumCard'
+import SmallCard from '../components/SmallCard'
+
+import { Exploredata, CardData } from '../typings'
+
+
+export default function Home({exploreData, cardsData}: {exploreData: Exploredata[], cardsData: CardData[]}) {
+  
   return (
     <div className="">
       <Head>
@@ -17,10 +25,63 @@ const Home: NextPage = () => {
       <Header />
 
       { /* Banner */}
+      <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+
+          {/* Pull some data from server - API endpoints */}
+
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {exploreData?.map((item: Exploredata)=>(
+            
+            <SmallCard  key={item.img} img={item.img} distance={item.distance} location={item.location} />
+          ))}
+
+          </div>
+
+          
+        </section>
+
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+
+          {/* Pull some data from server - API endpoints */}
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide  p-3 -ml-3 '>
+          {cardsData?.map((item:CardData)=>(
+            <MediumCard key={item.img} img={item.img} title={item.title}/>
+          ))}
+
+          </div>
+          
+        </section>
+        <LargeCard
+        img="https://a0.muscache.com/im/pictures/2da67c1c-0c61-4629-8798-1d4de1ac9291.jpg?im_w=1440"
+        title=" The Great Outdoors"
+        description="Wishlists curated by Airbnb."
+        buttonText="Get Inspired"/>
+
+   
+      </main>
 
       
     </div>
   )
 }
 
-export default Home
+export async function getStaticProps(){
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").then((res)=> res.json());
+
+  const cardsData = await fetch("https://www.jsonkeeper.com/b/VHHT").then((res)=> res.json())
+
+
+
+  return {
+    props:{
+      exploreData,
+      cardsData
+    }
+  }
+}
---
+++ typings.d.ts
@@ -0,0 +1,11 @@
+export type Exploredata = {
+    img:string;
+    location: string;
+    distance: string;
+}
+
+
+export type CardData ={
+    img:string;
+    title:string;
+}
